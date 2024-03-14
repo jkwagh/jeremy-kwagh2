c35314ffc20c85a702d9f2c15d245cd76d848d7d
Jeremy Kwagh <kwagh.jeremiah@gmail.com>
2024-03-14 16:15:01 -0400

    Site Completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,87 +5,115 @@ import FitnessEquip from "./assets/FitnessEquip.png";
 import FlatironEats from "./assets/FlatironEats.png";
 import Github from "./assets/GithubIcon.png"
 import Linkedin from "./assets/LinkedinIcon.png"
+import ArrowUp from './assets/arrow-up.svg';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [scrolling, setScrolling] = useState(false);
+
+  const onPageScroll = () => {
+    if(window.scrollY > 200) {
+      setScrolling(true)
+    } else {
+      setScrolling(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", onPageScroll)
+    return() => {
+      window.removeEventListener("scroll", onPageScroll)
+    }
+  }, [])
+
 
   return (
-    <>
-      <header>
-        <div className='container m-auto px-4 py-6'>
-          <div className='flex justify-between item-center'>
+    <div className='max-w-4xl m-auto relative'>
+      <header className={`${scrolling ? 'border-b border-gray-900' : ''} fixed left-0 right-0 top-0 z-20`} id='home'>
+        <div className='container m-auto px-4 py-6 max-w-4xl bg-black'>
+          <div className='flex flex-col gap-4 sm:flex-row justify-between item-center'>
             <div>
               <h1 className='font-bold text-2xl'>Jeremy Portfolio</h1>
             </div>
             <div>
               <ul className = 'flex gap-4'>
                 <li>
-                  <a className='text-gray-400 hover:text-white cursor-pointer'>Projects</a>
+                  <a href='#projects' className='text-gray-400 hover:text-white cursor-pointer'>Projects</a>
                 </li>
                 <li>
-                  <a className='text-gray-400 hover:text-white cursor-pointer'>Technologies</a>
+                  <a href="#technologies" className='text-gray-400 hover:text-white cursor-pointer'>Technologies</a>
                 </li>
                 <li>
-                  <a className='text-gray-400 hover:text-white cursor-pointer'>About Me</a>
+                  <a href="#aboutme" className='text-gray-400 hover:text-white cursor-pointer'>About Me</a>
                 </li>
               </ul>
             </div>
           </div>
         </div>  
       </header>
-      <main>
+      <main className='relative mt-28'>
         {/* Intro/Banner Section */}
         <section>
-          <div className='container m-auto px-4 py-32 flex gap-6'>
+          <div className='container m-auto px-4 pt-12 sm:py-32  flex flex-col sm:flex-row gap-6'>
             <div>
-              <h2 className='font-bold text-4xl'>Hello, I am Jeremy, </h2>
+              <h2 className='font-bold text-4xl'>Hello, I am Jeremy,</h2>
               <h2 className='font-bold text-4xl mt-1 gradiant-text'>Full-Stack Software Developer</h2>
               <p className='mt-4 text-gray-400'>Former accountant turned coder. Looking to apply years of corporate experience and understanding of business needs with coding skills.</p>
             </div>
             <div className='relative'>
-              <img src={Profile} width={320} className='relative z-1' alt='Photo of Jeremy Kwagh' ></img>
-              {/* <img src={ProfileBg} className='absolute top-0 left-10 -z-0'/> */}
+              <img src={Profile} className='relative z-10 w-[220px] m-auto sm:w-[520px]' alt='Photo of Jeremy Kwagh' ></img>
+              <div className='m-auto item-center'>
+                <ul className='flex mt-2 gap-2'>
+                  <li>
+                    <a href='https://github.com/jkwagh'><img src={Github} className='w-5' /></a>
+                  </li>
+                  <li>
+                    <a href='https://www.linkedin.com/in/jeremiahkwagh/'><img src={Linkedin} className='w-5'/></a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </section>
         {/* Projects Section */}
-        <section>
-          <div className='container m-auto px-4 py-12'>
+        <section id='projects'>
+          <div className='container m-auto px-4 sm:py-12'>
             <h2 className='text-2xl font-semibold'>
               Projects
             </h2>
-            <div className='flex gap-10 mt-10'>
-              <div className='border border-gray-500 rounded-md p-5'>
+            <div className='flex flex-col sm:flex-row gap-10 mt-11'>
+              <div className='border border-gray-500 rounded-md p-5 flex-1'>
                 <img src={EventPlanner} className='w-full h-auto'/>
                 <h3 className='text-2xl font-semibold mt-8'>Event Planner</h3>
                 <p className='text-gray-400 text-sm mt-2'>Fullstack web app for planning and organizing events</p>
-                <div className='flex justify-evenly mt-12'>
-                  <button className='px-6 py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hove:from-blue-700 hover:to-cyan-700'>Demo</button>
-                  <button className='px-6 py-3 border rounded-full hover:border-blue-500 hover:text-blue-500'>Github</button>
+                <div className='flex mt-12 gap-2'>
+                  <button className='flex-1   py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700'>Demo</button>
+                  <button className='flex-1 text-sm px-6 py-3 border rounded-full hover:border-blue-500 hover:text-blue-500'>Github</button>
                 </div>
               </div>
-              <div className='border border-gray-500 rounded-md p-5'>
+              <div className='border border-gray-500 rounded-md p-5 flex-1'>
                 <img src={FlatironEats} className='w-full h-auto'/>
                 <h3 className='text-2xl font-semibold mt-8'>Flatiron Eats</h3>
                 <p className='text-gray-400 text-sm mt-2'>Fullstack web app for ordering food and managing users</p>
-                <div className='flex justify-evenly mt-12'>
-                  <button className='px-6 py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hove:from-blue-700 hover:to-cyan-700'>Demo</button>
-                  <button className='px-6 py-3 border rounded-full hover:border-blue-500 hover:text-blue-500'>Github</button>
+                <div className='flex mt-12 gap-2'>
+                  <button className='flex-1 text-sm px-6 py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700'>Demo</button>
+                  <button className='flex-1 text-sm px-6 py-3 border rounded-full hover:border-blue-500 hover:text-blue-500'>Github</button>
                 </div>
               </div>
-              <div className='border border-gray-500 rounded-md p-5'>
+              <div className='border border-gray-500 rounded-md p-5 flex-1'>
                 <img src={FitnessEquip} className='w-full h-auto'/>
                 <h3 className='text-2xl font-semibold mt-8'>Fitness Equipment Marketplace</h3>
                 <p className='text-gray-400 text-sm mt-2'>React web app for viewing list of fitnesss equipment</p>
-                <div className='flex justify-evenly mt-12'>
-                  <button className='px-6 py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hove:from-blue-700 hover:to-cyan-700'>Demo</button>
-                  <button className='px-6 py-3 border rounded-full hover:border-blue-500 hover:text-blue-500'>Github</button>
+                <div className='flex mt-12 gap-2'>
+                  <button className='flex-1 text-sm px-6 py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700'>Demo</button>
+                  <button className='flex-1 text-sm px-6 py-3 border rounded-full hover:border-blue-500 hover:text-blue-500'>Github</button>
                 </div>
               </div>
             </div>
           </div>
           </section>
           {/* Technologies Section */}
-          <section className='mt-12'>
+          <section className="py-10" id="technologies">
           <div className='container m-auto px-4'>
             <h2 className='text-2xl font-semibold'>Technologies</h2>
             <div className='mt-14'>
@@ -131,34 +159,35 @@ function App() {
           <section className='py-10'>
             <div className='container m-auto px-4 py-14'>
             <h2 className='text-2xl font-semibold'>Additional Technologies and Skills</h2>
-            <div className='flex justify-between mt-12 w-[80%]'>
+            <div className='flex flex-col sm:flex-row gap-4 sm:gap-20 mt-12 w-[80%]'>
               <div>
-                <p className='font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-5 before:absolute relative left-3'>Git</p>
+                <p className='font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-3'>Git</p>
               </div>
               <div>
-                <p className='font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-5 before:absolute relative left-3'>Git</p>
+                <p className='font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-3'>Git</p>
               </div>
               <div>
-                <p className='font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-5 before:absolute relative left-3'>Git</p>
+                <p className='font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-3'>Git</p>
               </div>
               <div>
-                <p className='font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-5 before:absolute relative left-3'>Git</p>
+                <p className='font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-3'>Git</p>
               </div>
             </div>
-            <div className='flex justify-between mt-6 w-[80%]'>
+            <div className='flex flex-col sm:flex-row gap-4 sm:gap-20 mt-4 sm:mt-6 w-[80%]'>
               <div>
-                <p className='font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-5 before:absolute relative left-3'>Git</p>
+                <p className='font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-3'>Git</p>
               </div>
               <div>
-                <p className='font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-5 before:absolute relative left-3'>Git</p>
+                <p className='font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-3'>Git</p>
               </div>
               <div>
-                <p className='font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-5 before:absolute relative left-3'>Git</p>
+                <p className='font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-3'>Git</p>
               </div>
             </div>
             </div>
           </section>
-          <section className='py-8'>
+          {/* About Me Section */}
+          <section className='py-8' id='aboutme'>
             <div className='container m-auto px-4'>
               <h2 className='text-2xl font-semibold'>About Me</h2>
               <div className='mt-12 relative before:absolute before:top-0 before:left-16 before:rounded-full before:bottom-3 before:w-1 before:bg-white'>
@@ -183,25 +212,33 @@ function App() {
           </section>
       </main>
       <footer>
-        <div className='container m-auto flex justify-between'>
+        <div className='container m-auto flex justify-between px-4 py-6'>
           <div>
             <p className='text-gray-300 text-sm'>Copyright @ 2024</p>
           </div>
           <div>
             <ul className='flex gap-4'>
               <li>
-                <a><img src={Github} className='w-5' /></a>
+                <a href='https://github.com/jkwagh'><img src={Github} className='w-5' /></a>
               </li>
               <li>
-                <a><img src={Linkedin} className='w-5'/></a>
+                <a href='https://www.linkedin.com/in/jeremiahkwagh/'><img src={Linkedin} className='w-5'/></a>
               </li>
             </ul>
-
           </div>
         </div>
       </footer>
-    </>
-  )
+      {
+        scrolling && (
+          <button className="fixed block right-8 bottom-0 w-24" onClick={() => {
+            window.scrollTo(0,0);
+          }}>
+            <img src={ArrowUp} alt='Arrow Up' />
+          </button>
+        )
+      }
+    </div>
+  );
 }
 
-export default App
+export default App;
